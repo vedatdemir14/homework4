058d404fbc3e97a760867b7aa8e0928910db384f
Vedat Demir <vedatdemir14@hotmail.com>
2023-12-28 14:16:18 +0300

    Update Slime.java

--- Slime.java
+++ Slime.java
@@ -1,1 +1,35 @@
+import java.util.Random;
+public class Slime<T> extends Opponent<T>{
 
+    public Slime(int opponentId, int points, int attack, int speed, String opponentType, int attackFactor) {
+        super(opponentId, points, attack, speed, opponentType, attackFactor);
+
+        setOpponentType("Slime");
+
+        Random rand = new Random();
+        setPoints(rand.nextInt(50, 150));
+
+        setAttack(rand.nextInt(5, 25));
+
+        setSpeed(rand.nextInt(1, 90));
+
+
+
+
+    }
+
+        private static void absorb(){
+
+            int new_points = getPoints() + getAttack();
+            setPoints(Math.min(new_points, 150));
+            System.out.println("Slime used absorb ability!");
+            System.out.println("Slime's points increased by " + getAttack() + " points!");
+
+        }
+        public static void useAbsorb(){
+            absorb();
+        }
+
+
+
+    }
