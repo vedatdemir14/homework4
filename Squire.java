ee9d1757de62d44638b75e3228aa39bdebd1ea37
eraykocabozdogan <119955630+eraykocabozdogan@users.noreply.github.com>
2023-12-29 15:30:14 +0300

    Update Squire.java

--- Squire.java
+++ Squire.java
@@ -1,1 +1,18 @@
-
+package heroes;
+import java.util.Random;
+import java.util.Scanner;
+import Weapons.Weapon; 
+public class Squire<T> extends Human<T> {
+	
+	public Squire(String name, Weapon weapon) {
+		super(name, weapon);
+		
+	}
+	public void specialAction(Opponent<T> op) {
+		checkSpecialUsed();
+		setAttackFactor(0.5);
+		setStamina(10);
+		endTurn();
+	}
+	
+}
