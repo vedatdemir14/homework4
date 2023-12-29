6b79de99d51cc29fa69c06b64571275577cbd4dc
eraykocabozdogan <119955630+eraykocabozdogan@users.noreply.github.com>
2023-12-29 15:30:58 +0300

    Update Knight.java

--- Knight.java
+++ Knight.java
@@ -1,1 +1,16 @@
+package heroes;
+import java.util.Random;
 
+import Weapons.Weapon; 
+public class Knight<T> extends Human<T> {
+	
+	public Knight(String name, Weapon weapon) {
+		super(name, weapon);
+		// TODO Auto-generated constructor stub
+	}
+	public void specialAction(Opponent<T> op) {
+		checkSpecialUsed();
+		setAfNextTurn(3);
+		endTurn();
+	}
+}
