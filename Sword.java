e0b66616d093b6534bef7d03c9a0c9deaee0b669
DenizhanOngun <102353583+DenizhanOngun@users.noreply.github.com>
2023-12-29 23:08:23 +0300

    Update Sword.java

--- Sword.java
+++ Sword.java
@@ -1,1 +1,21 @@
+package characters;
+import java.util.Random;
 
+public class Sword extends Weapon {
+ public Sword() {
+	 super();
+ }
+
+ public  double attack1Rate() {
+    return 1;
+ }
+
+ public double attack2Rate() {
+     if (new Random().nextDouble() <= 0.25) {
+    	 System.out.print("Attack is failed.");
+    	 return 0;
+     } else {
+         return 2;
+     }
+ }
+}
