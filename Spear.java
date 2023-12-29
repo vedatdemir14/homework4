62d74620974600326d8255ef82f3ceaa32e4b624
DenizhanOngun <102353583+DenizhanOngun@users.noreply.github.com>
2023-12-29 23:08:08 +0300

    Update Spear.java

--- Spear.java
+++ Spear.java
@@ -1,1 +1,18 @@
+package characters;
+
+
+public class Spear extends Weapon {
+ public Spear() {
+     super();
+ }
+
+ public double attack1Rate() {
+     return 1.1;
+ }
+
+ public double attack2Rate(){
+     return 2;
+     //Skip Turn
+ }
+}
 
