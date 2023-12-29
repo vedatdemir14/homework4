770a825a1ec9bcddf362d76897346e7e96d15ec5
eraykocabozdogan <119955630+eraykocabozdogan@users.noreply.github.com>
2023-12-29 15:29:47 +0300

    Update Villager.java

--- Villager.java
+++ Villager.java
@@ -1,1 +1,12 @@
+package heroes;
+import java.util.Random;
+import java.util.Scanner;
+import Weapons.Weapon; 
+public class Villager<T> extends Human<T>{
 
+	public Villager(String name, Weapon weapon) {
+		super(name, weapon);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
