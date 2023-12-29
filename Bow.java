88cec6d171138deecac8944b36996031a63bb198
DenizhanOngun <102353583+DenizhanOngun@users.noreply.github.com>
2023-12-29 23:09:11 +0300

    Update Bow.java

--- Bow.java
+++ Bow.java
@@ -1,1 +1,18 @@
+package characters;
 
+public class Bow extends Weapon {
+ // Constructor
+ public Bow() {
+     super();
+ }
+
+ @Override
+ public double attack1Rate() {
+	 return 0.8;
+ }
+
+ @Override
+ public double attack2Rate() {
+	 return 2.5;
+ }
+}
