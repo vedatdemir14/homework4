73384438f3fa4f7aaeb9502498138b5a8fc57594
eraykocabozdogan <119955630+eraykocabozdogan@users.noreply.github.com>
2023-12-29 15:29:23 +0300

    Update Hunter.java

--- Hunter.java
+++ Hunter.java
@@ -1,1 +1,17 @@
-
+package heroes;
+import java.util.Random;
+import java.util.Scanner;
+import Weapons.Weapon; 
+public class Hunter<T> extends Human<T>{
+	
+	public Hunter(String name, Weapon weapon) {
+		super(name, weapon);
+		
+	}
+	public void specialAction(Opponent<T> op) {
+		checkSpecialUsed();
+		setAttackFactor(0.5);
+		doubleNextTurn();
+		endTurn();
+	}
+}
