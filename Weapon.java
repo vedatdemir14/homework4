92631c8e4f9673ba72324696a9061bc327af9e6c
DenizhanOngun <102353583+DenizhanOngun@users.noreply.github.com>
2023-12-29 23:08:38 +0300

    Update Weapon.java

--- Weapon.java
+++ Weapon.java
@@ -1,1 +1,20 @@
+package characters;
+import java.util.Random;
+public abstract class Weapon {
 
+	private  int additionalAttackBonus;
+	//Constructor
+	 public Weapon() {
+	        this.setAdditionalAttackBonus(new Random().nextInt(11) + 10); // Range: 10 to 20
+	    }
+	public  int getAdditionalAttackBonus() {
+		return additionalAttackBonus;
+	}
+	public abstract double attack1Rate();
+	
+	public  abstract double attack2Rate();
+	public void setAdditionalAttackBonus(int additionalAttackBonus) {
+		this.additionalAttackBonus = additionalAttackBonus;
+	}
+
+}
